0b82a8673dabbff9761c4e22298acf30e01b087c
sakeena11 <88400044+sakeena11@users.noreply.github.com>
2021-10-12 12:53:58 -0500

    Initial
    Created a picture of a cat and a tree using print

---
+++ AsciiArt.java
@@ -0,0 +1,15 @@
+public class AsciiArt {
+    public static void main(String[] args) {
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("   ***");
+        System.out.println();
+        System.out.println();
+        System.out.println(" /\\   /\\ ");
+        System.out.println("   o o");
+        System.out.println("  =   =");
+        System.out.println("   --- ");
+    }
+}
